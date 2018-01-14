0e312ba93686bf45e98c0c35eb25245a1b3f69c1
Ulrich Werner <ulrichwern@gmail.com>
2018-01-13 21:00:57 -0600

    Use real data

--- src/components/food/FoodList.js
+++ src/components/food/FoodList.js
@@ -9,68 +9,24 @@ export class FoodList extends Component {
     this.props.dispatch({type: 'USERS_FETCH_LIST'});
   }
   render() {
-    const columns = [{
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-      width: 150,
-      render: text => <a href="#">{text}</a>,
-    }, {
-      title: 'Age',
-      dataIndex: 'age',
-      key: 'age',
-      width: 70,
-    }, {
-      title: 'Address',
-      dataIndex: 'address',
-      key: 'address',
-    }, {
-      title: 'Action',
-      key: 'action',
-      width: 360,
-      render: (text, record) => (
-        <span>
-      <a href="#">Action 一 {record.name}</a>
-      <Divider type="vertical" />
-      <a href="#">Delete</a>
-      <Divider type="vertical" />
-      <a href="#" className="ant-dropdown-link">
-        More actions <Icon type="down" />
-      </a>
-    </span>
-      ),
-    }];
-    const data = [];
-    for (let i = 1; i <= 10; i++) {
-      data.push({
-        key: i,
-        name: 'John Brown',
-        age: `${i}2`,
-        address: `New York No. ${i} Lake Park`,
-        description: `My name is John Brown, I am ${i}2 years old, living in New York No. ${i} Lake Park.`,
-      });
-    }
-
-    const expandedRowRender = record => <p>{record.description}</p>;
-    const title = () => 'Here is title';
-    const showHeader = true;
-    const footer = () => 'Here is footer';
-    const scroll = { y: 240 };
-    this.state = {
-      bordered: false,
-      loading: false,
+    const columns = [
+      { title: 'name', dataIndex: 'name', key: 'name' },
+      { title: 'protein', dataIndex: 'protein', key: 'protein' },
+      { title: 'carbs', dataIndex: 'carbs', key: 'carbs' },
+      { title: 'fat', dataIndex: 'fat', key: 'fat' },
+      { title: 'serving size', dataIndex: 'servingSize', key: 'servingSize' },
+      { title: 'amount', dataIndex: 'amount', key: 'amount' },
+      ];
+    const tableConfig = {
+      bordered: true,
       pagination: true,
-      size: 'default',
-      expandedRowRender,
-      title,
-      showHeader,
-      footer,
+      size: 'small',
       rowSelection: {},
       scroll: undefined,
     };
     return (
       <div>
-        <Table {...this.state} columns={columns} dataSource={data} />
+        <Table {...tableConfig} columns={columns} dataSource={this.props.users} />
       </div>
     );
   }
